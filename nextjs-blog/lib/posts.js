b98a44c5efa14b4877b336e7da1ba49351378935
RishatMamat <reshatma@gmail.com>
2022-07-17 20:52:08 -0600

    Add post reader

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -5,5 +5,26 @@ import matter from 'gray-matter';
 const postsDirectory = path.join(process.cwd(), 'posts');
 
 export function getStoredPostsData() {
-    
+    const fileNames = fs.readdirSync(postsDirectory);
+    const allPostsData = fileNames.map((fileName) => {
+        const id = fileName.replace(/\.md$/, '');
+        const fullPath = path.join(postsDirectory, fileName);
+        const fileContent = fs.readFileSync(fullPath, 'utf8');
+        const matterResult = matter(fileContent);
+
+        return {
+            id, 
+            ...matterResult.data,
+        };
+    });
+
+    return allPostsData.sort(({date: a}, {data: b}) => {
+        if (a < b) {
+            return 1;
+        } else if (a > b) {
+            return -1;
+        } else {
+            return 0;
+        }
+    });
 }
