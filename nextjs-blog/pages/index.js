14cfd2db9d32b344c7a5571727ad9857b540d551
RishatMamat <reshatma@gmail.com>
2022-07-20 21:06:00 -0600

    Add pre-rendering of data

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,18 +1,34 @@
 import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getStoredPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getStoredPostsData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  }
+}
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section>
-        <p>Hey this is Richard a full stack Developer!</p>
-        <p>This is a sample website - you’ll be building a site like this on{' '}
-        <a href="https://nextjs.org/learn"> Our Next.js tutorial</a>        
-        </p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({id, date, title}) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout> 
   );
