600a9d52c12f3fdf570ec70c21af2d31b10c95d0
RishatMamat <reshatma@gmail.com>
2022-07-25 20:53:38 -0600

    Add dynamic routing

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -4,27 +4,39 @@ import matter from 'gray-matter';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 
-export function getStoredPostsData() {
+export function getStoredPostsIds() {
     const fileNames = fs.readdirSync(postsDirectory);
-    const allPostsData = fileNames.map((fileName) => {
-        const id = fileName.replace(/\.md$/, '');
-        const fullPath = path.join(postsDirectory, fileName);
-        const fileContent = fs.readFileSync(fullPath, 'utf8');
-        const matterResult = matter(fileContent);
+    // const allPostsData = fileNames.map((fileName) => {
+    //     const id = fileName.replace(/\.md$/, '');
+    //     const fullPath = path.join(postsDirectory, fileName);
+    //     const fileContent = fs.readFileSync(fullPath, 'utf8');
+    //     const matterResult = matter(fileContent);
 
+    //     return {
+    //         id, 
+    //         ...matterResult.data,
+    //     };
+    // });
+
+    // return allPostsData.sort(({date: a}, {data: b}) => {
+    //     if (a < b) {
+    //         return 1;
+    //     } else if (a > b) {
+    //         return -1;
+    //     } else {
+    //         return 0;
+    //     }
+    // });
+
+    return fileNames.map((fileName) => {
         return {
-            id, 
-            ...matterResult.data,
-        };
-    });
-
-    return allPostsData.sort(({date: a}, {data: b}) => {
-        if (a < b) {
-            return 1;
-        } else if (a > b) {
-            return -1;
-        } else {
-            return 0;
+            params: {
+                id: fileName.replace(/\.md$/, ''),
+            }
         }
-    });
+    })
+}
+
+export function getPostData() {
+    
 }
